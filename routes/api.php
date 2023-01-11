2e07414417941d37814508e1c1b527bcb416b8bd
reci-kelemen <kelemen.reka95@gmail.com>
2023-01-11 22:46:01 +0100

    created api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/manufacturers", function(){
+    $manufacturers = \App\Models\Buvar::all();
+    return response()->json($manufacturers);
+});
